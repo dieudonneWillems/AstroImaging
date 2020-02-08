dfbfbec74bdd60a61616caaa3f5da9c5c252bb91
Dieudonné Willems <don_willems@me.com>
2020-02-09 00:22:59 +0100

    Added first files

---
+++ AstroImageViewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AstroImageViewer
+//
+//  Created by Don Willems on 08/02/2020.
+//  Copyright © 2020 lapsedpacifist. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AstroImaging/Model/ASImage.swift
@@ -0,0 +1,13 @@
+//
+//  ASImage.swift
+//  AstroImaging
+//
+//  Created by Don Willems on 09/02/2020.
+//  Copyright © 2020 lapsedpacifist. All rights reserved.
+//
+
+import Foundation
+
+public class ASImage {
+    
+}
