96bc2080d7e72f2b2ad6db27a78c6e70c9084ad2
Dieudonné Willems <don_willems@me.com>
2020-02-08 23:53:09 +0100

    Initial Commit

---
+++ AstroImaging/AstroImaging.h
@@ -0,0 +1,19 @@
+//
+//  AstroImaging.h
+//  AstroImaging
+//
+//  Created by Don Willems on 08/02/2020.
+//  Copyright © 2020 lapsedpacifist. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AstroImaging.
+FOUNDATION_EXPORT double AstroImagingVersionNumber;
+
+//! Project version string for AstroImaging.
+FOUNDATION_EXPORT const unsigned char AstroImagingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AstroImaging/PublicHeader.h>
+
+
